4e56a3a787d54b9743db2ad420f8fef556a11bd9
Rort <thetrueroadman@gmail.com>
2022-10-23 13:20:25 -0400

    Added function to compare player and computer choice

--- rps.js
+++ rps.js
@@ -16,4 +16,41 @@ function getCompChoice() {
   }
 }
 
-console.log(getCompChoice());
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == "rock") {
+    if (computerSelection == "rock") {
+      return "tie";
+    } else if (computerSelection == "paper") {
+      return "lose";
+    } else if (computerSelection == "scissors") {
+      return "win";
+    }
+  }
+
+  if (playerSelection == "paper") {
+    if (computerSelection == "rock") {
+      return "win";
+    }
+    if (computerSelection == "paper") {
+      return "tie";
+    }
+    if (computerSelection == "scissors") {
+      return "lose";
+    }
+  }
+
+  if (playerSelection == "scissors") {
+    if (computerSelection == "rock") {
+      return "lose";
+    }
+    if (computerSelection == "paper") {
+      return "win";
+    }
+    if (computerSelection == "scissors") {
+      return "tie";
+    }
+  }
+}
+const playerSelection = "rock";
+const computerSelection = getCompChoice();
+console.log(playRound(playerSelection, computerSelection));

de7a2ac7884e0075bf1712a445cf3aaa2f102648
Rort <thetrueroadman@gmail.com>
2022-10-23 13:00:03 -0400

    Added Computer choosing mechanic

---
+++ rps.js
@@ -0,0 +1,19 @@
+function randomNum() {
+  return Math.floor(Math.random() * 3);
+}
+
+function getCompChoice() {
+  switch (randomNum()) {
+    case 0:
+      return "rock"
+      break;
+    case 1:
+      return "scissors";
+      break;
+    case 2:
+      return "paper";
+      break;
+  }
+}
+
+console.log(getCompChoice());

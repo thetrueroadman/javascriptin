e10af75a4c9b9eea54ce3e6f2fb2d2c36f15e62b
Rort <thetrueroadman@gmail.com>
2022-11-07 14:42:35 -0500

    Added output to show outcome of each round.

--- rps.js
+++ rps.js
@@ -17,55 +17,81 @@ function getCompChoice() {
 }
 let playerScore = 0;
 let computerScore = 0;
-function playRound(playerSelection) {
-  let computerSelection = getCompChoice();
 
+function playRound(playerSelection, computerSelection) {
   // 0 means tie, 1 means win, 2 means lose.
   if (playerSelection == "rock") {
     if (computerSelection == "rock") {
-      return "Tie";
+      return 0;
     } else if (computerSelection == "paper") {
       computerScore += 1;
-      return "Lose";
+      return 2;
     } else if (computerSelection == "scissors") {
       playerScore += 1;
-      return "Win";
+      return 1;
     }
   }
 
   if (playerSelection == "paper") {
     if (computerSelection == "rock") {
       playerScore += 1;
-      return "Win";
+      return 1;
     }
     if (computerSelection == "paper") {
-      return "Tie";
+      return 0;
     }
     if (computerSelection == "scissors") {
       computerScore += 1;
-      return "Lose";
+      return 2;
     }
   }
 
   if (playerSelection == "scissors") {
     if (computerSelection == "rock") {
       computerScore += 1;
-      return "Lose";
+      return 2;
     }
     if (computerSelection == "paper") {
       playerScore += 1;
-      return "Win";
+      return 1;
     }
     if (computerSelection == "scissors") {
-      return "Tie";
+      return 0;
     }
   }
 }
 
+//Makes sure the first letter of the output is capitalized.
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function outputSelect(buttonClick) {
+  let computerSelection = getCompChoice();
+  let x = playRound(buttonClick, computerSelection);
+  let ul = document.getElementById("output");
+  let li = document.createElement("li");
+  switch (x) {
+    case 0:
+      li.appendChild(document.createTextNode(`You both selected ${buttonClick}. Tie.`));
+      ul.prepend(li);
+      break;
+    case 1:
+      let outputWin = capitalize(`${buttonClick} beats ${computerSelection}. You win!`);
+      li.appendChild(document.createTextNode(outputWin));
+      ul.prepend(li);
+      break;
+    case 2:
+      let outputLose = capitalize(`${computerSelection} beats ${buttonClick}. You lose.`);
+      li.appendChild(document.createTextNode(outputLose));
+      ul.prepend(li);
+      break;
+  }
+}
 let rock = document.getElementById("rock");
 
 rock.addEventListener("click", e => {
-  console.log(playRound("rock"));
+  outputSelect("rock");
   document.getElementById("playerScore").innerHTML = `Player Score: ${playerScore}`;
   document.getElementById("compScore").innerHTML = `Computer Score: ${computerScore}`;
 });
@@ -73,7 +99,7 @@ rock.addEventListener("click", e => {
 let paper = document.getElementById("paper");
 
 paper.addEventListener("click", e => {
-  console.log(playRound("paper"));
+  outputSelect("paper");
   document.getElementById("playerScore").innerHTML = `Player Score: ${playerScore}`;
   document.getElementById("compScore").innerHTML = `Computer Score: ${computerScore}`;
 });
@@ -81,7 +107,7 @@ paper.addEventListener("click", e => {
 let scissors = document.getElementById("scissors");
 
 scissors.addEventListener("click", e => {
-  console.log(playRound("scissors"));
+  outputSelect("scissors");
   document.getElementById("playerScore").innerHTML = `Player Score: ${playerScore}`;
   document.getElementById("compScore").innerHTML = `Computer Score: ${computerScore}`;
 });
